fa1c191e84aeeb9bccfca36c8a62f51a0707a15e
KuanYangGH <ykxjtuedu@163.com>
2018-07-23 22:10:46 +0800

    完善数据预处理部分 text_image_dataset.py

--- test.py
+++ test.py
@@ -1,28 +1,16 @@
-import torch
-from torch.nn.functional import mse_loss 
+from dataset import load_dataset
+
 
 def main():
-    t1 = torch.FloatTensor([1,1])
-    t2 = torch.FloatTensor([4,1])
-    t3 = torch.tensor([7])
-    # t = [t1,t2,t3]
-    # # t = torch.stack(t,1)
-    # t = torch.cat(t,0)
-    # print(t)
-    # print(t.size())
-    t = t1*3+t2
-    print(t)
-    temp = torch.max(t1,t2)
-    print(temp)
-    temp = torch.sum(temp)
-    print(temp)
+    # cap = dset.CocoCaptions(root='F:\\COCO\\data\\mscoco2014\\train\\images',
+    #                         annFile='F:/COCO/data/mscoco2014/train/annotations/captions_train2014.json',
+    #                         transform=transforms.ToTensor())
 
-    print(mse_loss(t1,t2))
+    dset = load_dataset()
+    print(dset.size())
+    print(dset[0])
 
-    # a = torch.randn(1, 3)
-    # b = torch.sum(a)
-    # print(b)
-    # print(b.size())
 
 if __name__ == '__main__':
     main()
+
